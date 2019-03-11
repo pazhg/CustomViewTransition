b06826a12bb1487af2cd33825882a95dd9534276
Kambiz Guity <kg@pazhg.com>
2019-03-11 10:44:32 +0330

    Set MenuViewController to control new view

---
+++ CustomView/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  CustomView
+//
+//  Created by Pazhg on 12/20/1397 AP.
+//  Copyright © 1397 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
