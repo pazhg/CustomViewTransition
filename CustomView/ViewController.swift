350d9dd1330ac24bb0bd782640594d653f2bc6ef
Kambiz Guity <kg@pazhg.com>
2019-03-11 10:34:08 +0330

    Initial Commit

---
+++ CustomView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomView
+//
+//  Created by Pazhg on 12/20/1397 AP.
+//  Copyright © 1397 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
